2902251ae9797dd6775fa56c21b4fb0177f03d04
Trojan-Hawk <g00333333@gmit.ie>
2019-01-22 14:55:50 +0000

    Extract HSV skin tone values

--- gestureCapture.py
+++ gestureCapture.py
@@ -14,6 +14,10 @@ regionStartX = 250
 regionStartY = 130
 regionEndX = 470
 regionEndY = 350
+skinToneExtracted = 0
+
+output = []
+outputCount = 0
 
 # *************************************** TESTING ********************************************
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -27,106 +31,151 @@ while(1):
         frame = cv2.flip(frame, 1)
         kernel = np.ones((3, 3), np.uint8)
 
-        # *************************************** TESTING ********************************************
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # getting the mean skin colour of 100 samples
+        if outputCount > 10:
+            # setting the output to a numpy array
+            output = np.array(output)
+            print(output.astype(int))
+            # getting the mean hsv value
+            meanOutput = output.mean(axis=0)
+            # setting all values to whole numbers
+            meanOutput = meanOutput.astype(int)
+            # setting the upper and lower skin tone range
 
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
+            # set the boolean value to true
+            skinToneExtracted = 1
 
-        # Draw a rectangle around the faces
-        for (x, y, w, h) in faces:
-            # Define the area of the rectangle and the colour
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        # *************************************** Extracting Skin Tone ********************************************
+        if skinToneExtracted == 0:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # *************************************** TESTING ********************************************
-        
-        # define region of interest(Green Box)
-        roi = frame[regionStartX:regionEndX, regionStartY:regionEndY]
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
 
-        cv2.rectangle(frame, (regionStartY, regionStartX), (regionEndY, regionEndX), (0, 255, 0), 0)
-        hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in faces:
+                # Use this region to create a view of the extrapolated face
+                roi = frame[x:x + w, y:y + h]
+                # Show the area of the rectangle (Red Box)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                if roi is not None:
+                    # convert to HSV (Hue-Saturation-Value)
+                    hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+                    # display the HSV mask extracted from the area of interest
+                    # cv2.imshow('FaceHSV', hsv)
+                    # use colour quantization to extract the most common colours
+                    Z = roi.reshape((-1, 3))
+                    # convert to numpy float32
+                    Z = np.float32(Z)
+                    # define criteria, number of clusters(K) and apply kmeans() clustering
+                    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+                    K = 8
+                    ret, label, center = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+                    # convert back into uint8
+                    center = np.uint8(center)
+                    # flatten the pixles
+                    face1DArray = center[label.flatten()]
+                    # restore the original image shape
+                    face2DArray = face1DArray.reshape((roi.shape))
+                    # display the extracted face
+                    cv2.imshow('Quantization', face2DArray)
+                    # convert to a numpy array
+                    face2DArray = np.array(face2DArray)
+                    # calculate the mean hsv values for the 2D array
+                    mean = face2DArray.mean(axis=0)
+                    # calculate the mean of the mean, which gives us a single hsv value
+                    mean = mean.mean(axis=0)
+                    # append the hsv value to the output array
+                    output.append(mean)
+                    outputCount += 1
+                    print(outputCount)
+                    print(face1DArray)
 
-        # define range of skin color in HSV
-        lower_skin = np.array([0, 38, 80], dtype=np.uint8)
-        upper_skin = np.array([20, 255, 255], dtype=np.uint8)
+        # *************************************** Capturing Gesture ********************************************
+        else:
+            # define region of interest(Green Box)
+            roi = frame[regionStartX:regionEndX, regionStartY:regionEndY]
 
-        # extract skin colour image
-        mask = cv2.inRange(hsv, lower_skin, upper_skin)
+            cv2.rectangle(frame, (regionStartY, regionStartX), (regionEndY, regionEndX), (0, 255, 0), 0)
+            hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
 
-        # extrapolate the hand to fill dark spots within
-        mask = cv2.dilate(mask, kernel, iterations=2)
+            # extract skin colour image
+            mask = cv2.inRange(hsv, lower_skin, upper_skin)
 
-        # blur the image
-        mask = cv2.GaussianBlur(mask, (5, 5), 100)
+            # extrapolate the hand to fill dark spots within
+            mask = cv2.dilate(mask, kernel, iterations=2)
 
-        # Output of the visual data input (Camera)
-        # print(cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE))
+            # blur the image
+            mask = cv2.GaussianBlur(mask, (5, 5), 100)
 
-        # find contours
-        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            # Output of the visual data input (Camera)
+            # print(cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE))
 
-        # find contour of max area(hand)
-        cnt = max(contours, key=lambda x: cv2.contourArea(x))
-        
-        # skew the contours a little
-        epsilon = 0.0005*cv2.arcLength(cnt, True)
-        approx = cv2.approxPolyDP(cnt, epsilon, True)
+            # find contours
+            contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        # make a convex bounds around hand
-        hull = cv2.convexHull(cnt)
-        
-        # define area of bounds and area of hand
-        areahull = cv2.contourArea(hull)
-        areacnt = cv2.contourArea(cnt)
-      
-        # find the percentage of area not covered by hand in bounds
-        arearatio = ((areahull-areacnt)/areacnt)*100
-    
-        # find the defects in bounds with respect to hand
-        hull = cv2.convexHull(approx, returnPoints=False)
-        defects = cv2.convexityDefects(approx, hull)
-        
-        # l = no. of defects
-        defectCount = 0
-        
-        # code for finding no. of defects due to fingers
-        for i in range(defects.shape[0]):
-            s, e, f, d = defects[i, 0]
-            start = tuple(approx[s][0])
-            end = tuple(approx[e][0])
-            far = tuple(approx[f][0])
-            pt = (100, 180)
+            # find contour of max area(hand)
+            cnt = max(contours, key=lambda x: cv2.contourArea(x))
 
-            # find length of all sides of triangle
-            a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
-            b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
-            c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
-            s = (a+b+c)/2
-            ar = math.sqrt(s*(s-a)*(s-b)*(s-c))
-            
-            # distance between point and convex hull
-            d = (2*ar)/a
-            
-            # apply cosine rule here
-            angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
+            # skew the contours a little
+            epsilon = 0.0005*cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, epsilon, True)
 
-            # ignore angles > 90 and ignore points very close to convex hull(they generally come due to noise)
-            if angle <= 90 and d > 30:
-                defectCount += 1
-                cv2.circle(roi, far, 3, [255, 0, 0], -1)
-            
-            # draw lines around hand
-            cv2.line(roi, start, end, [0, 255, 0], 2)
+            # make a convex bounds around hand
+            hull = cv2.convexHull(cnt)
 
-        # display the black/white mask extracted from the area of interest
-        cv2.imshow('mask', mask)
-        # display the camera input data
-        cv2.imshow('frame', frame)
+            # define area of bounds and area of hand
+            areahull = cv2.contourArea(hull)
+            areacnt = cv2.contourArea(cnt)
+
+            # find the percentage of area not covered by hand in bounds
+            arearatio = ((areahull-areacnt)/areacnt)*100
+
+            # find the defects in bounds with respect to hand
+            hull = cv2.convexHull(approx, returnPoints=False)
+            defects = cv2.convexityDefects(approx, hull)
+
+            # l = no. of defects
+            defectCount = 0
+
+            # code for finding no. of defects due to fingers
+            for i in range(defects.shape[0]):
+                s, e, f, d = defects[i, 0]
+                start = tuple(approx[s][0])
+                end = tuple(approx[e][0])
+                far = tuple(approx[f][0])
+                pt = (100, 180)
+
+                # find length of all sides of triangle
+                a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
+                b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
+                c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
+                s = (a+b+c)/2
+                ar = math.sqrt(s*(s-a)*(s-b)*(s-c))
+
+                # distance between point and convex hull
+                d = (2*ar)/a
+
+                # apply cosine rule here
+                angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
+
+                # ignore angles > 90 and ignore points very close to convex hull(they generally come due to noise)
+                if angle <= 90 and d > 30:
+                    defectCount += 1
+                    cv2.circle(roi, far, 3, [255, 0, 0], -1)
+
+                # draw lines around hand
+                cv2.line(roi, start, end, [0, 255, 0], 2)
+
+            # display the black/white mask extracted from the area of interest
+            cv2.imshow('mask', mask)
+            # display the camera input data
+            cv2.imshow('frame', frame)
     except():
         print("Cannot access camera or none exists!")
         pass
